b64f28118fc4865233362bc2d6dd09ba932d65c7
Oleh <ctrlz777@gmail.com>
2022-01-26 22:11:39 +0200

    HW15 Django Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Like, Post
 
-# Register your models here.
+
+class TabularInlineLike(admin.TabularInline):
+    model = Like
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'updated_at')
+    inlines = [TabularInlineLike, ]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UniversalFields(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(UniversalFields):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=False, on_delete=models.SET_NULL
+        )
+
+    def __str__(self) -> str:
+        return f'{self.title}, {self.user}, {self.created_at}, {self.updated_at}'
+
+
+class Like(UniversalFields):
+    post = models.ForeignKey("Post", null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=False, on_delete=models.CASCADE
+        )
+    status = models.BooleanField(null=True, blank=True)
